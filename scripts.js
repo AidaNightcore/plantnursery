52adaf7b917b88fb819b285b12c141b1236dbb2f
Aida Elena Sentes <aidaelenasentes@gmail.com>
2023-05-15 02:15:13 +0300

    oke

--- scripts.js
+++ scripts.js
@@ -1,18 +1,18 @@
 function myFunction() {
   // Declare variables
-  var input, filter, ul, li, a, i;
+  let input, filter, plante[5], plante_nume[5], a, i;
   input = document.getElementById("mySearch");
   filter = input.value.toUpperCase();
-  ul = document.getElementById("myMenu");
-  li = ul.getElementsByTagName("li");
-
+  for (i = 0; i < 6; i++) plante[i] = document.getElementById(`plant${i}`);
+  for (i = 0; i < 6; i++)
+    plante_nume[i] = document.getElementById(`plante_nume${i}`);
   // Loop through all list items, and hide those who don't match the search query
-  for (i = 0; i < li.length; i++) {
-    a = li[i].getElementsByTagName("a")[0];
+  for (i = 0; i < plante.length; i++) {
+    a = plante_nume.getElementById("")[0];
     if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
-      li[i].style.display = "";
+      plante[i].style.display = "";
     } else {
-      li[i].style.display = "none";
+      plante[i].style.display = "none";
     }
   }
 }
